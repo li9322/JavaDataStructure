2055040ec3ea3038428a2ca51da21f9f3d7b5e28
li <1172892394@qqcom>
2021-04-06 20:45:05 +0800

    添加数组实现

---
+++ src/Array.java
@@ -0,0 +1,134 @@
+
+
+/**
+ * @DESCRIPTION:
+ * @USER: li
+ * @DATE: 2021/03/25 20:20
+ */
+public class Array<E> {
+    private E[] data;
+    private int size;
+
+    public Array(int capacity) {
+        data = (E[]) new Object[capacity];
+        size = 0;
+    }
+
+    public Array() {
+        data = (E[]) new Object[10];
+        size = 0;
+    }
+
+    public void add(int index, E e) {
+        if (index < 0 || index > size)
+            throw new IllegalArgumentException("插入位置非法");
+        if (size == data.length)
+            resize(2 * data.length);
+        for (int i = size; i > index; i--)
+            data[i] = data[i - 1];
+        data[index] = e;
+        size++;
+    }
+
+    public void addLast(E e) {
+        add(size, e);
+    }
+
+    public void addFirst(E e) {
+        add(0, e);
+    }
+
+    public void removeElement(E e) {
+        if (contains(e))
+            remove(find(e));
+    }
+
+    private void resize(int newCapacity) {
+        E[] newData = (E[]) new Object[newCapacity];
+        for (int i = 0; i < size; i++)
+            newData[i] = data[i];
+        data = newData;
+    }
+
+    public E remove(int index) {
+        if (index < 0 || index >= size)
+            throw new IllegalArgumentException("删除位置非法");
+        E result = data[index];
+        for (int i = index; i < size; i++)
+            data[i] = data[i + 1];
+        size--;
+        data[size] = null;
+        if (size == data.length / 4 && data.length / 2 != 0)
+            resize(data.length / 2);
+        return result;
+    }
+
+    public E removeFirst() {
+        return remove(0);
+    }
+
+    public E removeLast() {
+        return remove(size - 1);
+    }
+
+    public void update(int index, E e) {
+        if (index < 0 || index >= size)
+            throw new IllegalArgumentException("更新位置非法！");
+        data[index] = e;
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= size)
+            throw new IllegalArgumentException("查找位置非法！");
+        return data[index];
+    }
+
+    public boolean contains(E e) {
+        for (int i = 0; i < size; i++)
+            if (data[i].equals(e))
+                return true;
+        return false;
+    }
+
+    public int find(E e) {
+        for (int i = 0; i < size; i++)
+            if (data[i].equals(e))
+                return i;
+        return -1;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("Array:size = %d ,capacity = %d\n", size, data.length));
+        sb.append("[");
+        for (int i = 0; i < size - 1; i++) {
+            sb.append(data[i] + ", ");
+        }
+        sb.append(data[size - 1] + "]");
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Array<Integer> array = new Array<>();
+        array.addLast(1);
+        array.addLast(2);
+        array.addLast(3);
+        array.addLast(4);
+        System.out.println(array.toString());
+        array.add(1, 5);
+        System.out.println(array.toString());
+        array.addFirst(8);
+        System.out.println(array.toString());
+        System.out.println(array.contains(6));
+        System.out.println(array.contains(5));
+        System.out.println(array.find(5));
+        System.out.println(array.find(6));
+        array.removeElement(2);
+        System.out.println(array);
+        for (int i = 0; i < 10; i++) {
+            array.addLast(i);
+            System.out.println(array);
+        }
+    }
+}
